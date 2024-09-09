3d1a50bb5dda9c1fcc3b2d4d40f58b6d93a010d1
maxmurr <maxmurr.m@gmail.com>
2024-09-09 16:08:45 +0700

    update lib details

--- projects/byteark-player-angular/src/utils/update-player-props.ts
+++ projects/byteark-player-angular/src/utils/update-player-props.ts
@@ -1,5 +1,30 @@
-import isEqual from 'lodash.isequal';
 import type { ByteArkPlayer, ByteArkPlayerContainerProps } from '../types';
+
+function isEqual(a: any, b: any): boolean {
+  if (a === b) return true;
+
+  if (
+    typeof a !== 'object' ||
+    typeof b !== 'object' ||
+    a === null ||
+    b === null
+  ) {
+    return false;
+  }
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) return false;
+
+  for (const key of keysA) {
+    if (!keysB.includes(key) || !isEqual(a[key], b[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 type PlayerPropName =
   | 'autoplay'
